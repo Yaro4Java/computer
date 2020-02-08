977ae6b76c9ec3e314628ae29ab013f81fb9ad7e
Yaro4Java <kozlov_yv@mail.ru>
2020-02-08 23:43:15 +0300

    Initial commit

---
+++ src/com/company/Computer.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Computer {
+
+    public static void main(String[] args) {
+
+        System.out.println("This is just the very beginning of the project. A lot of things more have to be done.");
+
+    }
+}
